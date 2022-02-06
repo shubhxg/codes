0b805a2f0deb862a7b14e88e448d27edc0cfc8a2
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-02-06 17:34:23 +0530

    Update isSortedChecker.java

--- javaPractise/isSortedChecker.java
+++ javaPractise/isSortedChecker.java
@@ -1,9 +1,8 @@
-// import java.util.*;  
-class java {
+public class isSorted {
     public static void main(String[] args) {        
-        int[] arr = {1,2,4,6,8,2,3,1};
+        int[] arr = {1,2,3,4};
         boolean isSorted = true;
-        for (int i = 0; i<arr.length; i++) {
+        for (int i = 0; i<arr.length-1; i++) {
             if(arr[i] > arr[i+1]){
                 isSorted = false;
                 break;
