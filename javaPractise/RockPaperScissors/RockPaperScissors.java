6433d2dd47072e556c7b92a1f79a3a5ad23d1b54
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-02-13 23:32:47 +0530

    Update RockPaperScissors.java

--- javaPractise/RockPaperScissors/RockPaperScissors.java
+++ javaPractise/RockPaperScissors/RockPaperScissors.java
@@ -1,30 +1,65 @@
-// working on this 
-
-import java.util.Random;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        // driver code
         initiliseGame();
     }
 
     public static void initiliseGame() {
+
         final String INTRO = "-------ROCK PAPER SCISSOR GAME-------";
         System.out.println(INTRO);
-        System.out.println("Choose Rock or paper or scissors(r,p,s) : ");
 
         Scanner scanner = new Scanner(System.in);
-        String playerChoice = scanner.next();
+        String myMove = "";
+        int wins = 0, losses = 0;
 
-        switch (playerChoice) {
-            case "r" -> System.out.println("You have chosen Rock, Now CPU will choose!");
-            case "p" -> System.out.println("You have chosen Paper, Now CPU will choose!");
-            case "s" -> System.out.println("You have chosen Scissor, Now CPU will choose! ");
-            default -> System.out.println("Invalid option, please choose (r,p,s) only");
+        // game loop
+        while (true) {
+            System.out.print("Enter your move. Type in rock, paper or scissor. If you want to exit the game, type in exit : ");
+            myMove = scanner.next();
+
+            //checking if user entered exit
+            if (myMove.equals("exit")) {
+                break;
+            }
+
+            if (!myMove.equals("rock") && !myMove.equals("paper") && !myMove.equals("scissor")
+                    && !myMove.equals("exit")) {
+                System.out.println("Invalid move");
+            } else {
+
+                // Randomly generate cpu move between 0,1,2
+                int rand = (int) (Math.random() * 3);
+                String cpuMove = "";
+
+                if (rand == 0) {
+                    cpuMove = "rock";
+                } else if (rand == 1) {
+                    cpuMove = "paper";
+                } else {
+                    cpuMove = "scissor";
+                }
+                System.out.println("CPU move is : " + cpuMove);
+
+                // game logic
+                if (myMove.equals(cpuMove)) {
+                    System.out.println("Tied!\n");
+                } else if ((myMove.equals("rock") && (cpuMove.equals("scissor"))) ||
+                        (myMove.equals("scissors") && cpuMove.equals("paper")) ||
+                        (myMove.equals("paper") && cpuMove.equals("rock"))) {
+                    System.out.println("You won!\n");
+                    ++wins;
+                } else {
+                    System.out.println("You lost!\n");
+                    ++losses;
+                }
+            }
+
         }
-        do {
-            Random r = new Random();
-            char c = (char) (r.nextInt(26) + 'a');
-        } while (c != 'r' || c != 'p' || c != 's')
+        System.out.println("Thanks for playing :)");
+        System.out.println("You won " + wins + " times and lost " + losses + " times.");
     }
+
 }
