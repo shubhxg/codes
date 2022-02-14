5c538213b43d59064203132492ba349741927fb2
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-02-14 13:43:29 +0530

    Create firstGUI.java

---
+++ javaPractise/firstGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class firstGUI {
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are " + age + " year old");
+         
+    }
+}
