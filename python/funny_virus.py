cee0df299f3ada3b1145c56af786690aa786dda1
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-02-26 13:44:55 +0530

    Create funny_virus.py

---
+++ python/funny_virus.py
@@ -0,0 +1,17 @@
+import pyttsx3
+import os
+import time
+print("`r e c v i r u s` has been opened successfully...")
+time.sleep(2)
+print("`creating data dumpfiles in your system...`")
+time.sleep(2)
+i = 1
+while (i!= 5):
+    time.sleep(1)
+    print("creating dumps, initialising...")
+    i = i+1
+while i!=30:
+    print("y o u r s y s t e m h a s b e e n h a c k e d...")
+    os.system("start chrome")
+    time.sleep(1)
+    i = i+1
