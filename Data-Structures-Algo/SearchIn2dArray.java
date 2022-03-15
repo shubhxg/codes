9eb78285344fb3e9f12bad7c685052f09e882eac
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-03-15 18:26:08 +0530

    Create SearchIn2dArray.java

---
+++ Data-Structures-Algo/SearchIn2dArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Searchin2DArray {
+    public static void main(String[] args) {
+        
+        int[][] array = {{11,102}, 
+            {21,42,12,42,21}, 
+            {12,21}, 
+            {46,21,31,13,52,21}
+        };
+        int target = 31;
+
+        // returning the position of the element in array
+        int[] pos = search(array, target);
+        System.out.println(Arrays.toString(pos));
+    }
+
+    static int[] search(int[][] array, int target) {
+
+        // if array is empty return -1 in new array 
+        if(array.length == 0) return new int[]{-1};
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if(array[i][j] == target) return new int[]{i,j};
+            }
+            
+        }
+        // if not found
+        return new int[]{-1};
+    }
+}
