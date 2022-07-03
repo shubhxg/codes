df9e9a2d0454a48e9bd2ae0c213c7fb7de280883
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-07-03 15:26:17 +0530

    Create orderAgnosticBS.java

---
+++ ds-algo/orderAgnosticBS.java
@@ -0,0 +1,37 @@
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        // int[] arr = { 1, 4, 6, 7, 9, 12, 14 };
+        int[] arr = { 19, 12, 9, -1, -3 };
+        int target = -1;
+        System.out.println(orderAgnosticBS(arr, target));
+    }
+
+    static int orderAgnosticBS(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
+
+        // assigns true if array is ascending order and false if descending.
+        boolean isAsc = arr[s] < arr[e];
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            // if element found at mid returns the index
+            if (arr[mid] == target)
+                return mid;
+
+            if (isAsc) {
+                // array is ascending order sorted
+                if (target < arr[mid])
+                    e = mid - 1;
+                else
+                    s = mid + 1;
+            } else {
+                // array is descending order sorted
+                if (target > arr[mid])
+                    e = mid - 1;
+                else
+                    s = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
