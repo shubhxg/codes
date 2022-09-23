8623b0a6572af2b98b5f82c556fbb088d09a176f
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-09-23 21:18:48 +0530

    Update BinarySearch.java

--- ds-algo/BinarySearch.java
+++ ds-algo/BinarySearch.java
@@ -2,25 +2,22 @@ class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2,3,6,7,10,30,42,56};
         int target = 100;
-        System.out.println(binarySearch(arr, target));
+        System.out.println(binarySearch(arr, target, 0, array.length));
     }
 
-    private static int binarySearch(int[] arr, int target) {
-        int start = 0, end = arr.length-1;
+    private static int binarySearch(int[] arr, int target, int start, int end) {
         while (start<=end) {
             int mid = start+(end-start)/2;
+            
+//             checking at mid
+            if(array[mid] == key) return mid;
 
-            // target is bigger than mid element
-            if(target > arr[mid])
-                start = mid+1;
-            // target smaller than mid element
-            else if (target < arr[mid])
-                end = mid-1;
-            // target is at mid position
-            else return mid;
+//             checking in first half of array
+            if(key < array[mid]) end = mid-1;
+//             checking second half of array
+            else start = mid + 1;
         }
-        
-        // not found 
+//         not found 
         return -1;
     }
 }
