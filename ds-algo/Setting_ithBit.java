69a2920128ae11ab72eb7ed37ae37ccb65115450
Shubh Sharma <69891912+shubh-sharma19@users.noreply.github.com>
2022-10-01 15:25:46 +0530

    Create Setting_ithBit.java

---
+++ ds-algo/Setting_ithBit.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int n = 16; //10000
+        int mask = 1<<3; //1000
+        System.out.println(n|mask); //24
+    }
+}
